3f7292dfecafbd47da4e4b0f5fbbed26c3fae2b7
Andrew Shitov <mail@andreyshitov.com>
2024-07-15 18:03:14 +0200

    Scala

--- Scala/sleep-sort.scala
+++ Scala/sleep-sort.scala
@@ -2,15 +2,17 @@ import scala.concurrent.{Future, Await}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 
-val data = Array(10, 4, 2, 6, 5, 7, 1, 3)
+object SleepSort extends App {
+    val data = Array(10, 4, 2, 6, 5, 7, 1, 3)
 
-data.map(
-    (n) => {
-        Future {
-            Thread.sleep(n * 100)
-            println(n)
+    data.map(
+        (n) => {
+            Future {
+                Thread.sleep(n * 100)
+                println(n)
+            }
         }
-    }
-).map(
-    (f) => Await.result(f, Duration.Inf)
-)
+    ).map(
+        (f) => Await.result(f, Duration.Inf)
+    )
+}
